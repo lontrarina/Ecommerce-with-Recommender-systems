46429ecaf18ee671332ff69e53bc77d4889c7c34
karinaaduu@gmail.com <karinaaduu@gmail.com>
2024-05-29 15:50:42 +0300

    created models

--- app/models.py
+++ app/models.py
@@ -1,39 +1,63 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=False)
+    email = models.CharField(max_length=200, null=False)
 
+    def __str__(self):
+        return self.name
 
-# class Book(models.Model):
-#     title = models.CharField(max_length=200)
-#     author = models.CharField(max_length=100)
-#     publication_date = models.DateField()
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    category = models.CharField(max_length=50)
+    season= models.CharField(max_length=50)
+    styles= models.CharField(max_length=70) # boho\classic\sporty\casual
 
-#     def __str__(self):
-#         return self.title
+   # image = models.ImageField(null=True, blank=True)
 
-# class Users(models.Model):
-#     name = models.CharField(max_length=200)
-#     email = models.CharField(max_length=100)
-#     password = models.CharField(max_length=100)
-#     phone = models.CharField(max_length=100)
-#     address = models.CharField(max_length=100)
-
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
     
-# class Products(models.Model):
-#     name = models.CharField(max_length=200)
-#     description = models.CharField(max_length=100)
-#     price = models.CharField(max_length=100)
-#     image = models.CharField(max_length=100)
-
-#     def __str__(self):
-#         return self.name
     
-# class Orders(models.Model):
-#     user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
-#     product_id = models.ForeignKey(Products, on_delete=models.CASCADE)
-#     quantity = models.CharField(max_length=100)
-#     total_price = models.CharField(max_length=100)
-#     order_date = models.DateField()
+class InteractionHistory(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False)
+    date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        #return f"Customer: {self.customer.name}, Product: {self.product.name}"
+        return str(self.id)
+    
+
+
+class Cart(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, unique=True, null=False)
+
+    def __str__(self):
+        return str(self.id)
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, null=False)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=1)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
+
+class Wishlist(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, unique=True, null=False)
+
+    def __str__(self):
+        return str(self.id)
+    
+class WishlistItem(models.Model):
+    wishlist = models.ForeignKey(Wishlist, on_delete=models.CASCADE, null=False)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.id)
