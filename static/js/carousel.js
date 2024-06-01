4d4b5d0a8fcf661216a5aaca495d808c67050b17
karinaaduu@gmail.com <karinaaduu@gmail.com>
2024-06-01 16:48:35 +0300

    added carousel for products

--- static/js/carousel.js
+++ static/js/carousel.js
@@ -1,4 +1,12 @@
-// optional
-$('#blogCarousel').carousel({
-    interval: 5000
+  // Ініціалізація Slick Carousel після завантаження сторінки
+$(document).ready(function(){
+    $(".multiple-items").slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 5,
+      slidesToScroll: 2
+      
+    });
 });
+
